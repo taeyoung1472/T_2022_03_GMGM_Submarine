abb14dc523285f6c16f08f7f96f77af4f06928e5
sehyeon1104 <sehyeon1104@naver.com>
2022-07-06 19:44:33 +0900

    랜덤

--- Assets/01.Script/Dev/SeHyeon/RoomCtr/Room.cs
+++ Assets/01.Script/Dev/SeHyeon/RoomCtr/Room.cs
@@ -7,7 +7,10 @@ public abstract class Room : MonoBehaviour
     public int damageValue;
     public int hp = 1000;
     public int id = 0;
-    
+   protected virtual void RandomWall()
+    {
+
+    }
     protected abstract void OnCollisionEnter(Collision collision);
     
     
